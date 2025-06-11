2f7360c04639b1a5e668be1300ada317079d238d
Jannis Braun <jannis.braun@bluewin.ch>
2025-06-11 06:06:30 +0200

    seeded

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -1,7 +1,7 @@
 class Maker < ApplicationRecord
   belongs_to :user
   has_many :reviews
-  validates :category, presence: true
+  validates :categories, presence: true
   validates :name, presence: true
 
   CATEGORIES = ["meat", "seafood", "vegetables", "fruits", "dairy", "other", "drinks", "grains", "bakery & pastries", "eggs" ]
