e857a335503a444e173d972fc783041e304f7075
Alizee Driget <driget.alizee@gmail.com>
2025-06-11 11:52:49 +0800

    reviews controller - create method added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new
+    @maker = Maker.find(params[:maker_id])
+    @user = current_user
+    @review.user = @user
+    @review.maker = @maker
+    if @review.save
+      redirect_to maker_path(@maker)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
