d0de5261ee39dc65469025f337d9e4782edd8c8c
Clint Ryan <coderclint@gmail.com>
2025-06-12 10:37:33 +0800

    saving branch before merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @popular_makers = Maker.order(Arel.sql('RANDOM()')).limit(5)
   end
 end
--- app/models/maker.rb
+++ app/models/maker.rb
@@ -5,5 +5,13 @@ class Maker < ApplicationRecord
   validates :categories, presence: true
   validates :name, presence: true
 
-  CATEGORIES = ["meat", "seafood", "vegetables", "fruits", "dairy", "other", "drinks", "grains", "bakery & pastries", "eggs" ]
+  CATEGORIES = ["meat", "seafood", "vegetables", "fruits", "dairy", "other", "drinks", "grains", "bakery & pastries", "eggs"]
+
+  def average_rating
+    if reviews.present?
+      reviews.average(:overall_rating).round(2) # Changed to :overall_rating
+    else
+      "No reviews yet"
+    end
+  end
 end
