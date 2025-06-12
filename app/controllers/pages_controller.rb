fe4d88cd46706b209b2ac81180d06e189498359f
Clint Ryan <coderclint@gmail.com>
2025-06-12 15:50:50 +0800

    completed initial draft of pages#home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,11 @@ class PagesController < ApplicationController
 
   def home
     @popular_makers = Maker.order(Arel.sql('RANDOM()')).limit(5)
+
+    @high_rated_reviews = Review.where("overall_rating >= ?", 4.0)
+                                .order(created_at: :desc) # Or by overall_rating DESC, or randomly
+                                .limit(10) # Adjust the number of reviews you want to display
+
+    @high_rated_reviews = @high_rated_reviews.includes(:user, :maker)
   end
 end
