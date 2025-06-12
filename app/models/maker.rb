d5db555097003563c490c89136bdf66ce064947e
gyan singh <gyansinghmatta@gmail.com>
2025-06-12 14:35:45 +0800

    Changes to maker Model , Geocoding

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -5,7 +5,7 @@ class Maker < ApplicationRecord
   validates :categories, presence: true
   validates :name, presence: true
   geocoded_by :location
-  after_validation :geocode, if: :will_save_change_to_location?
+  # after_validation :geocode, if: :will_save_change_to_location?
 
   CATEGORIES = ["meat", "seafood", "vegetables", "fruits", "dairy", "other", "drinks", "grains", "bakery & pastries", "eggs" ]
 end
