63bd94eb9a40841af51e5f184982af18576a32d6
Clint Ryan <coderclint@gmail.com>
2025-06-12 16:20:21 +0800

    committing so that can merge with master

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :index ]
+  skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
     @popular_makers = Maker.order(Arel.sql('RANDOM()')).limit(5)
 
     @high_rated_reviews = Review.where("overall_rating >= ?", 4.0)
-                                .order(created_at: :desc) # Or by overall_rating DESC, or randomly
-                                .limit(10) # Adjust the number of reviews you want to display
+                                .order(created_at: :desc)
+                                .limit(10)
 
     @high_rated_reviews = @high_rated_reviews.includes(:user, :maker)
   end
