bfc892a0d14ee5973c6c3126adff9fee64dfbba7
Clint Ryan <coderclint@gmail.com>
2025-06-16 17:17:11 +0800

    changes to map functionality

--- app/controllers/makers_controller.rb
+++ app/controllers/makers_controller.rb
@@ -1,27 +1,15 @@
 class MakersController < ApplicationController
   def index
     @makers = Maker.all
-    if params[:name].present?
-      @makers = @makers.where("name ILIKE ?", "%#{params[:name]}%")
-    end
-    if params[:location].present?
-      @makers = @makers.where("location ILIKE ?", "%#{params[:location]}%")
-    end
-    if params[:category].present?
-      categories_params = params[:category].drop(1)
-      categories_params.each do |category|
-        @makers = @makers.select do |maker|
-          maker.categories.include?(category)
-        end
-      end
+    filter_makers
+    # redirect_to map_makers_path(request.query_parameters)
+
+    if params[:map_search].present? && params[:map_search] == 'true'
+      redirect_to map_makers_path(request.query_parameters)
+    else
+      # No explicit render needed, Rails will render index.html.erb by default
     end
 
-    # @markers = @makers.geocoded.map do |maker|
-    #   {
-    #     lat: maker.latitude,
-    #     lng: maker.longitude
-    #   }
-    # end
   end
 
   def show
@@ -46,18 +34,38 @@ class MakersController < ApplicationController
 
   def map
     @makers = Maker.all
+    filter_makers
     @markers = @makers.geocoded.map do |maker|
       {
         lat: maker.latitude,
-        lng: maker.longitude
-        # info_window_html: render_to_string(partial: "info_window", locals: {maker: maker })
+        lng: maker.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {maker: maker}),
+        marker_html: render_to_string(partial: "marker", locals: {maker: maker})
       }
     end
+    # redirect_to map_makers_path
   end
 
   private
 
   def maker_params
     params.require(:maker).permit(:name, :location, :description, categories: [], photos: [])
+  end
+
+  def filter_makers
+    if params[:name].present?
+      @makers = @makers.where("name ILIKE ?", "%#{params[:name]}%")
+    end
+    if params[:location].present?
+      @makers = @makers.where("location ILIKE ?", "%#{params[:location]}%")
+    end
+    if params[:category].present?
+      categories_params = params[:category].drop(1)
+      categories_params.each do |category|
+        @makers = @makers.select do |maker|
+          maker.categories.include?(category)
+        end
+      end
+    end
   end
 end
--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -12,30 +12,45 @@ export default class extends Controller {
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
-    this.map = new mapboxgl.Map({
+   this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10",
-      center: [115.188919, -8.409518], // [lng, lat] for Bali
-      zoom: 9 // Adjust zoom as needed for initial view of the island
-    })
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+
+    this.#fitMapToMarkers()
+
+    // Set a default center and zoom for Bali instead of fitting bounds
+    this.map.on('load', () => { // Ensure map is loaded before setting view
+      this.map.setCenter([115.137, -8.54]); // A more central point for Bali
+      this.map.setZoom(9); // Adjust this zoom level to zoom in/out
+    });
 
     this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+
   }
 
   #addMarkersToMap() {
-  this.markersValue.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([ marker.lng, marker.lat ])
-      // .setPopup(popup)
-      .addTo(this.map)
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+
+      // Create a HTML element for your custom marker
+      const customMarker = document.createElement("div")
+      customMarker.innerHTML = marker.marker_html
+
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map)
     })
   }
 
   #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds()
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    const baliBounds = [
+      [114.225, -8.823], // Southwest coordinates of Bali
+      [115.659, -8.044]  // Northeast coordinates of Bali
+    ];
+    this.map.fitBounds(baliBounds, { padding: 10, maxZoom: 25, duration: 0 });
   }
 
 }
