f7ec4d03436fd0a01e35bc55941fe79d9fa611f7
Alizee Driget <driget.alizee@gmail.com>
2025-06-17 14:54:00 +0800

    js stimulus animated numbers added

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,7 +2,7 @@
 
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
@@ -13,3 +13,4 @@ pin "tom-select" # @2.4.1
 
 pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@3.1.2/dist/mapbox-gl.js"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.1.0/nodelibs/browser/process-production.js"
+pin "@stimulus-components/animated-number", to: "https://ga.jspm.io/npm:@stimulus-components/animated-number@5.0.0/dist/stimulus-animated-number.mjs"
