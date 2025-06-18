f2e64e94bb7a92640e29ee98cb11c9c9238b4396
Clint Ryan <coderclint@gmail.com>
2025-06-18 11:55:36 +0800

    created a Transaction Log for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :makers, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :questions, dependent: :destroy
+  has_many :karma_transactions, dependent: :destroy
   validates :first_name, presence: true
   validates :last_name, presence: true
   # Include default devise modules. Others available are:
