35175346a6f6fa4a8c2acd468885db79cd0ae3f6
Clint Ryan <coderclint@gmail.com>
2025-06-18 09:07:14 +0800

    completed backend functionality for Karma

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def add_karma_points(action_type)
+    points = case action_type
+             when :maker_added
+               KarmaConfig::POINTS_FOR_ADDING_MAKER
+             when :review_submitted
+               KarmaConfig::POINTS_FOR_SUBMITTING_REVIEW
+             when :signed_up
+               KarmaConfig::POINTS_FOR_SIGNING_UP
+             else
+               0 # Default to 0 if the action_type is not recognized
+             end
+    increment!(:karma_points, points) if points > 0
+  end
+
+  after_create :set_initial_karma
+
+  private
+
+  def set_initial_karma
+    add_karma_points(:signed_up)
+  end
 end
